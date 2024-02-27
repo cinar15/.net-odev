db5d948b9aba5634d027ba5285e5617bc4d33c9b
cinar15 <125133778+cinar15@users.noreply.github.com>
2024-02-27 09:51:44 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using react_native.Models;
+
+namespace react_native.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Ulkeler()
+    {
+        string[ ] country ={
+            "Azerbaycan",
+            "Rusya",
+            "Almanya",
+        };
+        ViewBag.countries=country;
+        return View();
+
+    }
+
+    public IActionResult Cnr()
+    {
+        return View();
+    }
+
+    public IActionResult Time()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace react_native.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
